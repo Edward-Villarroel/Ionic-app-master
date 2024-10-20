cde47d9f3314ea9ca9c1256829f920ef77750e94
aywey <yeanleyton6@gmail.com>
2024-10-20 10:49:41 -0300

    modified:   src/app/component/popupformulario/popupformulario.component.scss 	modified:   src/app/component/popupformulario/popupformulario.component.ts

--- src/app/component/popupformulario/popupformulario.component.ts
+++ src/app/component/popupformulario/popupformulario.component.ts
@@ -3,31 +3,31 @@ import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-popupformulario',
-  templateUrl: './popupformulario.component.html', // Asegúrate de que este archivo exista
+  templateUrl: './popupformulario.component.html', 
   styleUrls: ['./popupformulario.component.scss']
 })
 export class PopupFormulario implements OnInit {
-  @Input() tiempoRestante: number = 10; // Inicializa con un valor por defecto
-  tiempo: number = this.tiempoRestante; // Variable para almacenar el tiempo actual
-  intervalo: any; // Para manejar el intervalo de cuenta regresiva
+  @Input() tiempoRestante: number = 10; 
+  tiempo: number = this.tiempoRestante; 
+  intervalo: any; 
 
   constructor(private modalController: ModalController) {}
 
   ngOnInit() {
-    this.iniciarCuentaRegresiva(); // Inicia la cuenta regresiva
+    this.iniciarCuentaRegresiva(); 
   }
 
   iniciarCuentaRegresiva() {
     this.intervalo = setInterval(() => {
       this.tiempo--;
       if (this.tiempo <= 0) {
-        this.cerrar(); // Cierra el popup cuando el tiempo se agote
+        this.cerrar(); 
       }
     }, 1000);
   }
 
   cerrar() {
-    clearInterval(this.intervalo); // Detiene el intervalo
-    this.modalController.dismiss(); // Cierra el popup
+    clearInterval(this.intervalo); 
+    this.modalController.dismiss(); 
   }
 }
