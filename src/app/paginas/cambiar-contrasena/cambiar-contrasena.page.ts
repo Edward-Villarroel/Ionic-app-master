ebc9cd81972dd9e146785ad5690be422aaada37a
Edward-Villarroel <edward1313212192@gmail.com>
2024-10-29 12:10:55 -0300

    olvide guardar XD

---
+++ src/app/paginas/cambiar-contrasena/cambiar-contrasena.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cambiar-contrasena',
+  templateUrl: './cambiar-contrasena.page.html',
+  styleUrls: ['./cambiar-contrasena.page.scss'],
+})
+export class CambiarContrasenaPage {
+  passwordForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = this.fb.group({
+      currentPassword: ['', Validators.required],
+      newPassword: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordsMatch });
+  }
+
+  
+  passwordsMatch(formGroup: FormGroup) {
+    const newPassword = formGroup.get('newPassword')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    return newPassword === confirmPassword ? null : { notMatching: true };
+  }
+
+  
+  onSubmit() {
+    if (this.passwordForm.valid) {
+ 
+      console.log('Contraseña cambiada con éxito');
+    }
+  }
+
+
+  clearForm() {
+    this.passwordForm.reset();
+  }
+}
